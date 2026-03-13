4b4f891c1f8f8a4f7eb799f0642ff1b488038495
Zefrod <clarence.crozier@proton.me>
2026-03-13 13:35:34 +0100

    Update 4_interrogations.sql

--- 4_interrogations.sql
+++ 4_interrogations.sql
@@ -50,6 +50,25 @@ FROM Produit P
 JOIN Modele M ON P.Modele_ID = M.Modele_ID
 WHERE M.Modele_nom = 'Pegasus';
 
+--Prix moyen par poids de chaussures
+SELECT 
+    C.Chaussure_poids AS Poids_en_grammes, 
+    ROUND(AVG(P.Produit_prix), 2) AS Prix_Moyen
+FROM Chaussure C
+JOIN Produit P ON C.Produit_ID = P.Produit_ID
+GROUP BY C.Chaussure_poids
+ORDER BY C.Chaussure_poids ASC;
+
+--Prix total et nombre de chaussures par modèle
+SELECT 
+    M.Modele_nom, 
+    COUNT(C.Chaussure_ID) AS Nombre_de_Chaussures, 
+    SUM(P.Produit_prix) AS Valeur_Totale_Stock
+FROM Modele M
+JOIN Produit P ON M.Modele_ID = P.Modele_ID
+JOIN Chaussure C ON P.Produit_ID = C.Produit_ID
+GROUP BY M.Modele_nom
+ORDER BY Valeur_Totale_Stock DESC;
 
 --Partie 3 : jointures internes, externes, simples, multiples
 --Détails des différents t-shirt avec leur nom de modèle et leur matière
@@ -59,7 +78,62 @@ JOIN Produit P ON T.Produit_ID = P.Produit_ID
 JOIN Modele M ON P.Modele_ID = M.Modele_ID
 JOIN Categorie CAT ON P.Categorie_ID = CAT.Categorie_ID;
 
+--Liste de tous les modèles avec la marque, la catégorie et le prix
+SELECT M.Modele_nom, MAR.Marque_nom, C.Categorie_nom, P.Produit_prix
+FROM Produit P
+JOIN Modele M ON P.Modele_ID = M.Modele_ID
+JOIN Marque MAR ON M.Marque_nom = MAR.Marque_nom
+JOIN Categorie C ON P.Categorie_ID = C.Categorie_ID;
+
+--Liste complète des caractéristiques de toutes les chaussures
+SELECT M.Modele_nom, CH.Chaussure_terrain, CH.Chaussure_poids, CH.Chaussure_drop, CH.Chaussure_type_foulee, CH.Chaussure_profondeur_crampon, CH.Chaussure_distance,CH.Chaussure_type_semelle,CH.Chaussure_impermeabilite
+FROM Chaussure CH
+JOIN Produit P ON CH.Produit_ID = P.Produit_ID
+JOIN Modele M ON P.Modele_ID = M.Modele_ID;
 
 
 --Partie 4 :  requêtes imbriquées
+--Liste des modèles dont le prix est supérieur à la moyenne des prix
+SELECT DISTINCT M.Modele_nom, P.Produit_prix
+FROM Modele M
+JOIN Produit P ON M.Modele_ID = P.Modele_ID
+WHERE P.Produit_prix > (SELECT AVG(Produit_prix) FROM Produit);
+
+--Liste des modèles des catégories t-shirts et shorts
+SELECT Modele_nom 
+FROM Modele 
+WHERE Modele_ID IN (
+    SELECT Modele_ID FROM Produit WHERE Categorie_ID IN (
+        SELECT Categorie_ID FROM Categorie WHERE Categorie_nom IN ('T-shirts', 'Shorts')
+    )
+);
+
+--Liste des modèles qui sont des shorts
+SELECT Modele_nom 
+FROM Modele 
+WHERE Modele_ID IN (
+    SELECT Modele_ID 
+    FROM Produit 
+    WHERE Categorie_ID = (SELECT Categorie_ID FROM Categorie WHERE Categorie_nom = 'Shorts')
+);
+
+--Liste des modèles de chaussures qui sont plus lourdes que les chaussures de route
+SELECT DISTINCT M.Modele_nom
+FROM Modele M
+JOIN Produit P ON M.Modele_ID = P.Modele_ID
+JOIN Chaussure C ON P.Produit_ID = C.Produit_ID
+WHERE C.Chaussure_poids > ALL (
+    SELECT Chaussure_poids FROM Chaussure WHERE Chaussure_terrain = 'Route'
+);
+
+--Liste des modèles qui sont moins chère que n'importe quelle chaussure Salomon
+SELECT DISTINCT M.Modele_nom
+FROM Modele M
+JOIN Produit P ON M.Modele_ID = P.Modele_ID
+WHERE P.Produit_prix < ANY (
+    SELECT P2.Produit_prix 
+    FROM Produit P2 
+    JOIN Modele M2 ON P2.Modele_ID = M2.Modele_ID 
+    WHERE M2.Marque_nom = 'Salomon'
+);
 
